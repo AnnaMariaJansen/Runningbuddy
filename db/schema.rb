e19b645d03949f17ebaeeb9b7171f812e9077333
Sophia Denney <sophiad14@kabelmail.de>
2020-11-30 13:15:29 +0100

    lots of work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_153648) do
+ActiveRecord::Schema.define(version: 2020_11_30_100626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,15 +35,15 @@ ActiveRecord::Schema.define(version: 2020_11_25_153648) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.bigint "meeting_id_id"
-    t.bigint "user_id_id"
-    t.string "vibe_rating"
-    t.string "route_rating"
-    t.string "challenge_rating"
+    t.bigint "meeting_id"
+    t.bigint "user_id"
+    t.integer "vibe_rating"
+    t.integer "route_rating"
+    t.integer "challenge_rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["meeting_id_id"], name: "index_reviews_on_meeting_id_id"
-    t.index ["user_id_id"], name: "index_reviews_on_user_id_id"
+    t.index ["meeting_id"], name: "index_reviews_on_meeting_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "runs", force: :cascade do |t|
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_153648) do
     t.date "birthday"
     t.string "gender"
     t.integer "running_level"
+    t.string "avatar_url", default: "https://www.vippng.com/png/detail/416-4161690_empty-profile-picture-blank-avatar-image-circle.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
