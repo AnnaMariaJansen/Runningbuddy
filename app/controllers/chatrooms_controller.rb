3dc7ea26b351cfdaec478c32fd1bacf3251667a2
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2020-12-02 16:43:07 +0100

    donWE

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,14 +3,14 @@ class ChatroomsController < ApplicationController
   def index
     all_buds = policy_scope(BuddyConnection)
     @buddies = all_buds.where(user_1_id: current_user) + all_buds.where(user_2_id: current_user)
-    @chatrooms = policy_scope(Chatroom)
     @messages_unread = []
+    @chatrooms = policy_scope(Chatroom)
+    # chatroom ordering:
     @chatrooms.each do |chat|
       Message.where(chatroom_id: chat.id).each do |message|
         @messages_unread << message unless message.read
       end
     end
-    # limit to only show chatrooms current_user is a part of
   end
 
   def show
