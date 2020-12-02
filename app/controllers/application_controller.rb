3183c9c3f57a97518486a48978b8e54ee28a3d5e
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2020-12-02 11:25:33 +0100

    dot works lets see hwat else we can do

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :count_unreads
 
   include Pundit
   # Pundit: white-list approach.
@@ -14,6 +15,21 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def count_unreads
+    all_buds = policy_scope(BuddyConnection)
+    @buddiesapp = all_buds.where(user_1_id: current_user) + all_buds.where(user_2_id: current_user)
+    @chatroomsapp = []
+    @buddiesapp.each do |buddy|
+      @chatroomsapp << policy_scope(Chatroom).where(buddy_connection_id: buddy.id)
+    end
+    @messages_unreadapp = []
+    @chatroomsapp.each do |chat|
+      Message.where(chatroom: chat).each do |message|
+        @messages_unreadapp << message unless message.read || message.user_id == current_user.id
+      end
+    end
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
---
+++ app/javascript/packs/unread.js
@@ -0,0 +1,18 @@
+
+const unreadCounter = () => {
+  const messageDot = document.querySelector(".message-dot")
+
+  let unreadMessagesnumber = document.getElementById("unread").innerText
+  parseInt(unreadMessagesnumber, 10)
+  console.log(unreadMessagesnumber);
+
+  if (unreadMessagesnumber > 0) {
+    messageDot.classList.remove("hidden");
+  } else {
+    messageDot.classList.add("hidden");
+  };
+};
+
+
+
+export { unreadCounter }
