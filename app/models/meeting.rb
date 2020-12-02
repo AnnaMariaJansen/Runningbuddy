c7d1ab8ca4bd9f7041a6b31205a8dc0fe5cb4159
Sophia Denney <sophiad14@kabelmail.de>
2020-12-02 17:22:45 +0100

    SEARCH ANYTHING YOU FUCKING WANT

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :user, foreign_key: "user_id"
   belongs_to :run
+
   has_many :reviews, dependent: :destroy
   validates :status, inclusion: { in: ["pending", "confirmed", "cancelled"] }
 end
